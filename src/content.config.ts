228da286a23567d7f5f078e48956cea12dc95450
cloudflypeng <cloudcflypeng@gmail.com>
2025-05-22 19:28:53 +0800

    fix: 更新

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,11 +1,11 @@
 import { defineCollection } from 'astro:content'
-import { postLoader } from 'sakuin/astro'
+import { postLoader, shortsLoader } from 'sakuin/astro'
 
 const posts = defineCollection({
   loader: postLoader({ handle: 'meanc-9641' }),
 })
 
-export const collections = { posts }
+// export const collections = { posts }
 
 const shorts = defineCollection({
   loader: shortsLoader({ handle: 'meanc-9641' }),
